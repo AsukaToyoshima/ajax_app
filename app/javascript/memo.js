350781f4d17072b08a68eea8a8b83bce185f24ee
AsukaToyoshima <asuka.toyoshima@di-v.co.jp>
2020-12-20 13:01:09 +0900

    a

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,7 @@
 function memo() {
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
-    const formData = new FormData(document.getElementById("farm"));
+    const formData = new FormData(document.getElementById("form"));
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
